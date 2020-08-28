c7588ac2a1bcf548e59c489439494d97fbc93cd0
karan <karanraj17111996@gmail.com>
2020-08-28 15:21:07 +0530

    added overview and commercial and residential buildings

--- js/script.js
+++ js/script.js
@@ -1,11 +1,35 @@
+// const btn = document.getElementById('enquire-btn');
+
+
+
+// window.onscroll = fun;
+
+// function fun() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         console.log("object");
+//         btn.style.top = "35%";
+//     } else {
+//         console.log("object no");
+//     }
+// }
+
+
+// fire the menu button
 $(document).ready(function () {
     $(".button a").click(function () {
         $(".overlay").fadeToggle(200);
         $(this).toggleClass('btn-open').toggleClass('btn-close');
     });
+    // Close enquiry form
+    $('#enquire-btn').click(() => {
+        $('#enquire-container').fadeToggle(300);
+    });
+    // menu button close
+    $('.overlay').on('click', function () {
+        $(".overlay").fadeToggle(200);
+        $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+        open = false;
+    });
 });
-$('.overlay').on('click', function () {
-    $(".overlay").fadeToggle(200);
-    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-    open = false;
-});
+
+
