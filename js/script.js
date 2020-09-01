5e2916f761a09998df820d509958677987afe7fb
karan <karanraj17111996@gmail.com>
2020-09-01 17:04:07 +0530

    added count animation and AOS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,59 @@
+$(document).ready(function () {
+
+
+    // Owl carousel
+    $('.hero-slider').owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        animateOut: 'fadeOut',
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        loop: true
+    });
+
+    // Menu button
+    $(".button a").click(function () {
+        $(".overlay").fadeToggle(200);
+        $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+
+    // menu button close
+    $('.overlay').on('click', function () {
+        $(".overlay").fadeToggle(200);
+        $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+        open = false;
+    });
+
+    // Close enquiry form
+    $('#enquire-btn').click(() => {
+        $('#enquire-container').fadeToggle(300);
+    });
+
+    // project type hover
+    $('.media::after').hover(() => {
+        $('.media::after').css('display', 'none');
+    })
+    $('.com').hover(() => {
+        console.log("hovering com");
+        $('.media::after').css('display', 'none');
+    });
+
+    // Counter
+    var countSection = $('#number-counter');
+    countSection.waypoint(function () {
+        $('.count').each(function () {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 5000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    }, { offset: '50%' });
+});
 // const btn = document.getElementById('enquire-btn');
 
 
@@ -12,51 +68,3 @@
 //         console.log("object no");
 //     }
 // }
-
-
-// fire the menu button
-$(document).ready(function () {
-    $('.hero-slider').owlCarousel({
-        items: 1,
-        nav: true,
-        dots: false,
-        animateOut: 'fadeOut',
-        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-        loop: true
-    });
-
-    $(".button a").click(function () {
-        $(".overlay").fadeToggle(200);
-        $(this).toggleClass('btn-open').toggleClass('btn-close');
-    });
-    // Close enquiry form
-    $('#enquire-btn').click(() => {
-        $('#enquire-container').fadeToggle(300);
-    });
-    // menu button close
-    $('.overlay').on('click', function () {
-        $(".overlay").fadeToggle(200);
-        $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-        open = false;
-    });
-    // project type hover
-    $('.media::after').hover(() => {
-        $('.media::after').css('display', 'none');
-    })
-    $('.com').hover(() => {
-        console.log("hovering com");
-        $('.media::after').css('display', 'none');
-    });
-
-    $('.count').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
-    });
-});
