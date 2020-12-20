974ca87adb148dddd1b63b526c49b15e53015c26
karan <karanraj17111996@gmail.com>
2020-12-20 14:04:58 +0530

    added projects page

--- js/script.js
+++ js/script.js
@@ -59,7 +59,15 @@ $(document).ready(function () {
         return false;
     });
 });
-
+jQuery(function () {
+    jQuery('#showall').click(function () {
+        jQuery('.targetDiv').show();
+    });
+    jQuery('.showSingle').click(function () {
+        jQuery('.targetDiv').hide();
+        jQuery('#div' + $(this).attr('target')).show();
+    });
+});
 
 
 
